0b6f26a8997225311df86dd4b857454612b43f76
vitaly2193 <vitaly2193@gmail.com>
2022-01-22 14:58:25 +0300

    task-03-limit-size-stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -1,13 +1,25 @@
+const { log } = require('console');
 const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.encoding = options.encoding;
+    this.size = 0;
+  }
+
+  getStrSize(str) {
+    return Buffer.from(str).length;
   }
 
   _transform(chunk, encoding, callback) {
-
+    const str = chunk.toString(this.encoding);
+    const size = this.getStrSize(str);
+    this.size += size;
+    const exceeded = this.size > this.limit;
+    callback(exceeded ? new LimitExceededError() : null, exceeded ? null : str);
   }
 }
 
