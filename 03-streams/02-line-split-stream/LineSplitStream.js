56e9bc60d83c1d45a867229bb7ee7ab9904f6c44
vitaly2193 <vitaly2193@gmail.com>
2022-01-23 14:08:13 +0300

    task-03-split-stream, task-02 - pass buffer to getting size func

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,24 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.incoming = '';
+    this.encoding = options.encoding;
   }
 
   _transform(chunk, encoding, callback) {
+    this.incoming += chunk.toString(this.encoding);
+    const exploded = this.incoming.split(os.EOL);
+    const last = exploded.length - 1;
+    exploded.forEach((item, index) => {
+      index === last ? this.incoming = exploded[last] : this.push(item);
+    });
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.incoming);
+    this.incoming = '';
+    callback();
   }
 }
 
