da1aa59a1b699e8d49bbdacddca85e942e3841c1
vitaly2193 <vitaly2193@gmail.com>
2022-01-25 12:46:59 +0300

    task-07-file-server-delete

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -10,9 +11,31 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname.indexOf('/') >= 0) {
+    res.statusCode = 400;
+    res.end('Subfolders are not supported.');
+  }
+
+  const deleteFile = async (fpath) => {
+    try {
+      await fs.promises.access(fpath, fs.constants.R_OK | fs.constants.W_OK);
+      fs.unlink(fpath, (error) => {
+        if (error) {
+          res.statusCode = 500;
+          res.end('Server error.');
+        }
+        res.statusCode = 200;
+        res.end('Deleted!');
+      });
+    } catch {
+      res.statusCode = 404;
+      res.end('File not exists.');
+    }
+  }
+
   switch (req.method) {
     case 'DELETE':
-
+      deleteFile(filepath);
       break;
 
     default:
