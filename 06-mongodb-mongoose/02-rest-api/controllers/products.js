ddb93247901a15a8d29b135b014eb08472c061a1
vitaly2193 <vitaly2193@gmail.com>
2022-02-06 21:34:50 +0300

    task-10-rest-api-products

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,36 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
+  const productsRaw = await Product.find({subcategory: subcategory});
+  const products = productsRaw.map(product => mapProduct(product));
 
-  ctx.body = {};
+  ctx.body = {products};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const productsRaw = await Product.find({});
+  const products = productsRaw.map(product => mapProduct(product));
+
+  ctx.body = {products};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const id = ctx.params.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    ctx.throw(400, 'Product ID is not in correct format.');
+  }
+
+  const product = await Product.findById(id);
+
+  if (!product) {
+    ctx.throw(404, 'Product can not be found.');
+  }
+  ctx.body = {product: mapProduct(product)};
 };
 
