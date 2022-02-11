71f97db48a828586039cc2bbef1e5b61779f9a87
vitaly2193 <vitaly2193@gmail.com>
2022-02-11 11:36:36 +0300

    task-13-auth-github

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,22 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  try {
+    if (!email) {
+      return done(null, false, 'Не указан email');
+    }
+    const user = await User.findOne({email});
+    if (user) {
+      return done(null, user);
+    }
+    const newUser = new User({ email, displayName });
+    await newUser.save();
+    const newUserDoc = await User.findOne({email});
+    if (newUserDoc) {
+      return done(null, newUserDoc);
+    }
+    done(null, false, 'Аутентификация не удалась.');
+  } catch (err) {
+    done(err);
+  }
 };
