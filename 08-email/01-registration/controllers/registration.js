d1130246b1571baffe163681909d42530af87cde
vitaly2193 <vitaly2193@gmail.com>
2022-02-16 18:18:13 +0300

    task-15-registration

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -3,9 +3,40 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+    try {
+        const token = uuid();
+        const {email, displayName, password} = ctx.request.body;
+        const newUser = await User.create({
+            email,
+            displayName,
+            verificationToken: token,
+        });
+        await newUser.setPassword(password);
+        await newUser.save();
+        await sendMail({
+            template: 'confirmation',
+            locals: {token: 'token'},
+            to: email,
+            subject: 'Подтвердите почту',
+        });
 
+        ctx.status = 200;
+        ctx.body = { status: 'ok' };
+    } catch (err) {
+        ctx.throw(400, err);
+    }
 };
 
 module.exports.confirm = async (ctx, next) => {
+    try {
+        const {verificationToken} = ctx.request.body;
+        const user = await User.findOne({verificationToken});
+        user.verificationToken = undefined;
+        await user.save();
 
+        ctx.status = 200;
+        ctx.body = {token: verificationToken};
+    } catch (err) {
+        ctx.throw(400, err);
+    }
 };
